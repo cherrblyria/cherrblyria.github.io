db37562a0befbab32592b508f47f3c11fda89ab2
cherrblyria <cherrblyria@gmail.com>
2026-07-24 19:37:39 +0700

    feat: favicon

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const birthDate = new Date('2011-09-12'); 
+const birthDate = new Date('2011-09-12');
 const today = new Date();
 
 let age = today.getFullYear() - birthDate.getFullYear();
 const monthDiff = today.getMonth() - birthDate.getMonth();
 
 if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
-    age--;
+  age--;
 }
 
 const ageSpan = document.querySelector('.age');
 if (ageSpan) {
-    ageSpan.textContent = `${age} years old`;
+  ageSpan.textContent = `${age} years old`;
 }
